b8ece055c39bb94cf557c766125053dd465310e3
Borntorule007 <84105603+Borntorule007@users.noreply.github.com>
2021-05-27 18:50:28 +0530

    Create Password Generator.py

---
+++ Password Generator.py
@@ -0,0 +1,8 @@
+# Password Generator
+import random
+passlen = int(input("enter the length of password"))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s,passlen ))
+print(p)
+
+# Password Generated based on the length using random variables
