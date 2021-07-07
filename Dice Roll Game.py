5df00b79fe71c0cffa0138aa2f8ed2a1370d7c78
Borntorule007 <84105603+Borntorule007@users.noreply.github.com>
2021-07-07 23:46:52 +0530

    Update Dice Roll Game.py

--- Dice Roll Game.py
+++ Dice Roll Game.py
@@ -24,3 +24,58 @@ while roll_again == "yes" or roll_again == "y":
     
     #asking user to roll the dice again. Any input other than yes or y will terminate the loop
     roll_again = input("Roll the Dices Again?") 
+    
+    
+    
+    
+    or 
+    
+    
+    #Rolling dice in a graghical way
+    
+    import random
+print("This is a dice stimulator")
+x = "y"
+
+while x == "y":
+    number = random.randint(1,6)
+
+    if number == 1:
+        print("----------")
+        print("|        |")
+        print("|    O   |")
+        print("|        |")
+        print("----------")
+    if number == 2:
+        print("----------")
+        print("|        |")
+        print("| O    O |")
+        print("|        |")
+        print("----------")
+    if number == 3:
+        print("----------")
+        print("|    O   |")
+        print("|    O   |")
+        print("|    O   |")
+        print("----------")
+    if number == 4:
+        print("----------")
+        print("| O    O |")
+        print("|        |")
+        print("| O    O |")
+        print("----------")
+    if number == 5:
+        print("----------")
+        print("| O    O |")
+        print("|    O   |")
+        print("| O    O |")
+        print("----------")
+    if number == 6:
+        print("----------")
+        print("| O    O |")
+        print("| O    O |")
+        print("| O    O |")
+        print("----------")
+    x = input("Press y to roll again")
+    
+
